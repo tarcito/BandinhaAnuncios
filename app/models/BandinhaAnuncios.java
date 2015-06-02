7efbda8cf7cd99273ca9ca313a1357caa97e9ba0
tarcito <tarcito.lua@gmail.com>
2015-06-02 11:16:20 -0300

    adicionado getNumeroDeAnuncios e getAnuncios em BandinhaAnuncios

--- app/models/BandinhaAnuncios.java
+++ app/models/BandinhaAnuncios.java
@@ -19,6 +19,19 @@ public class BandinhaAnuncios {
     @Transactional
     public void adicionaAnuncio(String titulo, String descricao, String cidade, String bairro, List<String> instrumentos, List<String> estilosQGosta, List<String> estilosQNaoGosta, boolean objetivo, List<String> formaDeContato) throws Exception {
         Anuncio novoAnuncio = new Anuncio(titulo, descricao, cidade, bairro, instrumentos, estilosQGosta, estilosQNaoGosta, objetivo, formaDeContato);
+        repositorioAnuncios.persist(novoAnuncio);
+        repositorioAnuncios.flush();
+    }
 
+    
+
+    @Transactional
+    public List<Anuncio> getAnuncios(){
+        return  repositorioAnuncios.findAll();
+    }
+
+    @Transactional
+    public long getNumerodeAnuncios(){
+        return repositorioAnuncios.countAll();
     }
 }
